c2720c2c3fc143abe9bbe3c5644f13967088b657
Asidrus <asidrus@mail.ru>
2021-09-20 16:27:42 +0300

    v 1.6.3

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from config import *
 import asyncio
+from matplotlib import pyplot as plt
+import random
+from datetime import datetime, timedelta
 
 
 async def main(msg):
@@ -10,7 +13,18 @@ async def main(msg):
     await writer.wait_closed()
 
 
+def main2():
+    timings = []
+    for i in range(10):
+        timings.append(timedelta(seconds=random.randint(0, i*2)))
+    print(timings)
+    plt.plot([time.seconds for time in timings])
+    plt.savefig("../temp/pic1.png")
+    # plt.show()
 
-
+from datetime import time
 if __name__ == "__main__":
-    asyncio.run(main(msg))
+    # asyncio.run(main("hi"))
+    # main2()
+    time(second=1)
+    time.
