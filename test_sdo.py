bcd1c193e1459b854fc38c0faec5dbb57d31207b
Asidrus <asidrus@mail.ru>
2021-10-22 10:16:59 +0300

    v 1.8.0

--- test_sdo.py
+++ test_sdo.py
@@ -126,7 +126,7 @@ def test_sdo(setup_driver, write_log, clicker):
                      _alarm=f"{severity}: {suite_name}: {test_name}: Проблема с загрузкой {mainUrl}"):
         driver.get(mainUrl)
         gatherBrowserLogs(driver)
-        if checkStatus[driver.requests[0].response.status_code]:
+        if not checkStatus[driver.requests[0].response.status_code]:
             raise Exception(f"Ответ от сервера:{driver.requests[0].response.status_code}")
     with allure_step(f"Вход в личный кабинет", driver=driver, screenshot=True, browser_log=True,
                      _alarm=f"{severity}: {suite_name}: {test_name}:"):
