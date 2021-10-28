c600e0089dc03a1a85a61f3e9733e525d7aa150b
Asidrus <asidrus@mail.ru>
2021-10-28 11:29:30 +0300

    v 1.8.5

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from libs.form import DataToXpath, Form
@@ -11,12 +14,14 @@ def process_browser_log_entry(entry):
 
 
 def main():
-    driver = webdriver.Chrome("./resources/chromedriver")
+    options = webdriver.ChromeOptions()
+    options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
+    # options.set_capability('goog:loggingPrefs', {'browser': 'ALL'})
+    driver = webdriver.Chrome("./resources/chromedriver", options=options)
+
     try:
-        options = webdriver.ChromeOptions()
-        options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
-        options.set_capability('goog:loggingPrefs', {'browser': 'ALL'})
         mainUrl = "https://sdo.niidpo.ru/login/index.php"
+        time1 = datetime.datetime.now()
         driver.get(mainUrl)
         name = driver.find_element("xpath", "//input[@name='username']")
         password = driver.find_element("xpath", "//input[@name='password']")
@@ -24,11 +29,21 @@ def main():
         name.send_keys(listener_login)
         password.send_keys(listener_password)
         button.click()
-        logs = driver.get_log('performance')
-        print(logs)
+        logs = driver.get_log("performance")
         events = [process_browser_log_entry(entry) for entry in logs]
         events = [event for event in events if 'Network.response' in event['method']]
-        print(events)
+        reqs = []
+        for e in events:
+            try:
+                if "https://sdo.niidpo.ru/" == e["params"]["response"]["url"] or "https://sdo.niidpo.ru/login/index.php" == e["params"]["response"]["url"]:
+                    reqs.append(datetime.datetime.fromtimestamp(e["params"]["response"]["responseTime"]/1000))
+            except:
+                pass
+        print(reqs[1]-reqs[0])
+        print(datetime.datetime.now()-reqs[1])
+
+        # with open("tmp2.json", "w") as file:
+        #     json.dump({"data": reqs}, file, indent=4)
 
     except Exception as e:
         raise e
