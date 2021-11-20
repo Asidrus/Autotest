d4225f8e88d5890f978eabb9719c1738b73b994d
Asidrus <asidrus@mail.ru>
2021-11-20 05:20:53 -0500

    kali home

---
+++ libs/network.py
@@ -0,0 +1,180 @@
+import asyncio
+import sys
+
+
+class Protocol:
+    """
+    protocol v 0.1
+    text and image transmission
+    """
+    header = [{'head': "message", "data": {"len": 4, "offset": 0, "value": None}},
+              {'head': "text", "data": {"len": 4, "offset": 4, "value": None}},
+              {'head': "image", "data": {"len": 8, "offset": 8, "value": None}}]
+
+    raw = b''
+
+    STOP_READING = False
+    serial = 0
+    data = {}
+
+    def __headerLength__(self):
+        count = 0
+        for head in self.header:
+            count = count + head['data']['len']
+        return count
+
+    def __bodyLength__(self):
+        length = 0
+        for head in self.header:
+            if head['head'] != "message":
+                length = length + head['data']['value']
+        return length
+
+    def set(self, key, value):
+        for head in self.header:
+            if head['head'] == key:
+                head['data']['value'] = value
+                return None
+
+    def setData(self, text: bytes = b'', image: bytes = b''):
+        self.set("text", len(text))
+        self.set("image", len(image))
+        self.set("message", self.__headerLength__() + self.__bodyLength__())
+        for head in self.header:
+            self.raw = self.raw + (head['data']['value']).to_bytes(head['data']['len'], byteorder='big')
+        self.raw = self.raw + text + image
+        return self.raw
+
+    def getData(self, s: bytes):
+        for head in self.header:
+            offset = head['data']['offset']
+            length = head['data']['len']
+            self.set(head['head'], int.from_bytes(s[offset:offset + length], 'big'))
+        bl = self.__headerLength__()
+        cursor = bl
+        for head in self.header:
+            if head['head'] != 'message':
+                length = head['data']['value']
+                self.data[head['head']] = s[cursor:cursor + length]
+                cursor = cursor + length
+
+    def setChunk(self, chunk):
+        if self.serial == 0:
+            self.raw = chunk
+            offset = self.header[0]['data']['offset']
+            length = self.header[0]['data']['len']
+            self.header[0]['data']['value'] = int.from_bytes(self.raw[offset:offset + length], 'big')
+            self.serial = self.serial + 1
+        else:
+            self.raw = self.raw + chunk
+            self.serial = self.serial + 1
+        if len(self.raw) >= self.header[0]['data']['value']:
+            self.STOP_READING = True
+            self.getData(self.raw)
+
+
+class Server:
+    ip: str = None
+    port: int = None
+    handler = None
+
+    def __init__(self, ip='localhost', port=1234, handler=None):
+        self.ip = ip
+        self.port = port
+        self.handler = handler
+
+    async def serveClient(self, reader, writer):
+        request = await readMessage(reader)
+        if request is None:
+            print(f'Client unexpectedly disconnected')
+        else:
+            if self.handler is None:
+                pass
+            else:
+                response = await self.handler(**request)
+                await writeMessage(writer, **response)
+                writer.close()
+
+    async def runSever(self):
+        server = await asyncio.start_server(self.serveClient, self.ip, self.port)
+        await server.serve_forever()
+
+
+class Client:
+    handler = None
+    ip = None
+    port = None
+
+    def __init__(self, ip='localhost', port=1234, handler=None):
+        self.ip = ip
+        self.port = port
+        self.handler = handler
+
+    async def send(self, **kwargs):
+        while True:
+            try:
+                reader, writer = await asyncio.open_connection(self.ip, self.port)
+            except Exception as e:
+                print(f'Couldn`t connect to {self.ip}:{self.port}')
+                return False
+            await writeMessage(writer, **kwargs)
+            data = await readMessage(reader)
+            print(data)
+            if self.handler is None:
+                writer.close()
+                break
+            else:
+                res = await self.handler(**data)
+                if res == True:
+                    writer.close()
+                    return True
+                elif res == False:
+                    continue
+                else:
+                    writer.close()
+                    return False
+
+
+async def writeMessage(writer, **kwargs):
+    protocol = Protocol()
+    writer.write(protocol.setData(**kwargs))
+
+
+async def readMessage(reader) -> dict:
+    protocol = Protocol()
+    while not protocol.STOP_READING:
+        chunk = await reader.read(2 ** 10)
+        if not chunk:
+            break
+        protocol.setChunk(chunk)
+    return protocol.data
+
+
+async def handlerIn(**kwargs):
+    print(kwargs['text'].decode())
+    if kwargs['text'].decode() == 'test':
+        return {'text': b'ok'}
+    else:
+        return {'text': b'error'}
+
+
+async def handlerOut(**kwargs):
+    print(kwargs['text'].decode())
+    if kwargs['text'].decode() == 'huy':
+        return {'text': 'ok'}
+    else:
+        return {'text': 'error'}
+
+
+if __name__ == "__main__":
+    print(sys.argv[1])
+    if sys.argv[1] == '1':
+        print('start server')
+        server = Server(handler=handlerIn)
+        loop = asyncio.new_event_loop()
+        loop.create_task(server.runSever())
+        loop.run_forever()
+    else:
+        print('start client')
+        client = Client()
+        asyncio.run(client.send(text=b'test'))
