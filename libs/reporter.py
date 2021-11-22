3ef817ea1ef9dca44dae04e1ca1ce462172711f8
Asidrus <asidrus@mail.ru>
2021-11-22 04:16:56 -0500

    kali home

---
+++ libs/reporter.py
@@ -0,0 +1,76 @@
+import asyncio
+from contextlib import contextmanager
+from time import time
+
+import allure
+from allure_commons.types import AttachmentType
+
+from libs.network import Client
+from libs.webdriver import WebDriver
+
+class Reporter:
+
+    header = ''
+    logger = None
+    webdriver = None
+    driver = None
+    telegram = None
+
+    screenshot = None
+
+    def __init__(self, header='',
+                 logger=None,
+                 webdriver: WebDriver = None,
+                 telegram: Client = None,
+                 debug = False):
+        self.header = header
+        self.logger = logger
+        self.webdriver = webdriver
+        if self.webdriver:
+            self.driver = webdriver.driver
+        self.telegram = telegram
+
+    def takeScreenshot(self):
+        if self.driver:
+            self.screenshot = self.driver.get_screenshot_as_png()
+
+    def gatherBrowserLogs(self):
+        if self.logger and self.webdriver and self.webdriver.logs:
+            self.logger.warning(
+                {"url": self.driver.current_url,
+                 "messages": self.driver.get_log('browser')})
+
+    def sendToTelegram(self, stepName, error):
+        try:
+            d = {'text': f"{self.header}\nШаг {stepName} провален\nОшибка:\n{str(error)[:30]}",
+                 'datetime': time(),
+                 'debug': 0}
+            data = {"text": str(d).encode()}
+            if self.screenshot:
+                data['image'] = self.screenshot
+            asyncio.run(self.telegram.send(**data))
+        except:
+            if self.logger:
+                self.logger.critical(f"Не смог отправить сообщение в ТГ")
+
+    @contextmanager
+    def step(self, stepName: str,
+             screenshot: bool = False,
+             browserLog: bool = False,
+             alarm: bool = False,
+             ignore: bool = False):
+        with allure.step(stepName):
+            try:
+                yield
+            except Exception as e:
+                self.takeScreenshot()
+                if screenshot and self.driver:
+                    allure.attach(self.screenshot, name=stepName, attachment_type=AttachmentType.PNG)
+                if browserLog:
+                    self.gatherBrowserLogs()
+                if alarm:
+                    self.sendToTelegram(stepName, e)
+                if self.logger:
+                    self.logger.critical(f"{self.header}|{stepName}|" + str(e))
+                if ignore is not True:
+                    raise Exception(e)
