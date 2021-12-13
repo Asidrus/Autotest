4d4d3511fafc053b9d693455a9cafbc8962bdbaf
Asidrus <asidrus@mail.ru>
2021-12-13 17:04:46 +0300

    v 1.9.1

--- libs/search_content.py
+++ libs/search_content.py
@@ -1,21 +1,26 @@
+import asyncio
+import aiohttp
+import os
 import sys
-import pathlib
-path = str(pathlib.Path(__file__).parent.parent.resolve())
-sys.path.insert(-1, path)
+sys.path.append('/home/kali/autotest/')
 import json
 from io import StringIO
-import aiohttp
 from lxml import etree
-from config import autotest_results
-import asyncio
-from aioparser import aioparser
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+from config import *
+STORAGE_PATH = autotest_results+'/'
 
-
-# autotest_results = "/home/kali/autotest-results"
-
-headers = {
-    "User-Agent": 'Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166'}
-fname_appendix = ""
+def putInDict(url, link, dictionary):
+    flag = False
+    for _link in dictionary:
+        if _link["url"] == url:
+            flag = True
+            if link["url"] not in _link["from"]:
+                _link["from"].append(link["url"])
+            break
+    if not flag:
+        dictionary.append({"url": url, "from": [link["url"]]})
 
 
 def find_all(a_str, sub):
@@ -27,61 +32,162 @@ def find_all(a_str, sub):
         start += len(sub)
 
 
-async def searcher(links, pattern, encoding):
-    res = {}
-    for p in pattern:
-        res[p] = []
+headers = {
+    "User-Agent": 'Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166'}
+
+
+class aioparser:
+    parser = None
+    site = ''
+    links = {"internal": [], "external": [], "resources": [], "errors": []}
+    result = {}
+    adaptive = False
+    fnameLinks = ''
+    fnameResults = ''
+    autosave = True
+
+    def __init__(self, site, pattern=None, adaptive=False, parse=False, autosave=True):
+        inds = [ind for ind in find_all(site, '/')]
+        if len(inds) > 2:
+            self.site = site[:[ind for ind in find_all(site, '/')][2]]
+        else:
+            self.site = site
+        self.adaptive = adaptive
+        self.headers = headers if adaptive else None
+        self.parse = parse
+        self.pattern = pattern
+        self.autosave = autosave
+        self.parser = etree.HTMLParser()
+        self.getFileName()
+
+    def __str__(self):
+        return f"site='{self.site}', pattern='{self.pattern}'"
+
+    async def run(self):
+        if os.path.exists(self.fnameLinks) and ( #and (not self.pattern)
+                (datetime.now() - datetime.fromtimestamp(os.path.getmtime(self.fnameLinks))) < timedelta(hours=23)):
+            self.readfile(self.fnameLinks)
+        else:
+            print(
+                f"Файл {self.fnameLinks} не найден или страрый, начинаем парсинг ссылок, наливайте кофе... это надолго")
+            self.links = {"internal": [], "external": [], "resources": [], "errors": []}
+            self.links['internal'] = [{"url": self.site, "from": []}]
+            self.result = {}
+            for p in self.pattern:
+                self.result[p] = []
+            await self.parsing()
+            self.writeResults()
+
+    def writeResults(self):
+        self.writefile(self.fnameLinks, self.links)
+        if self.pattern:
+            self.writefile(self.fnameResults, self.result)
+
+    def getFileName(self):
+        start = [ind for ind in find_all(self.site, '/')][1] + 1
+        end = [ind for ind in find_all(self.site, '.')][-1]
+        domain = self.site[start:end] + "_adaptive" if self.adaptive == True else self.site[start:end]
+        self.fnameLinks = STORAGE_PATH + domain + "_links.json"
+
+        if (type(self.autosave) == bool) and (self.autosave == True):
+            self.fnameResults = STORAGE_PATH + domain + "_result.json"
+        else:
+            self.fnameResults = STORAGE_PATH + self.autosave + '.json'
+
+    def readfile(self, fname):
+        with open(fname, "r") as read_file:
+            self.links = json.load(read_file)
+
+    def writefile(self, fname, data):
+        with open(fname, "w", encoding='utf-8') as write_file:
+            json.dump(data, write_file, indent=4, ensure_ascii=False)
+            print(f'сохранил файл в {fname}')
+
+    def takeLink(self):
+        yield from self.links['internal']
+
+    async def parsing(self):
+        async with aiohttp.ClientSession() as session:
+            for link in self.takeLink():
+                #
+                # if 'seminar' not in link["url"]:
+                #     continue
+                #
+                print(link["url"])
+                try:
+                    async with session.get(link["url"], headers=headers) as response:
+                        header = response.headers["Content-Type"]
+                        if "text/html" not in header:
+                            continue
+                        encoding = header[header.find("=") + 1:]
+                        try:
+                            html = await response.text(encoding, errors="ignore")
+                        except Exception as e:
+                            html = await response.text('windows-1251', errors="ignore")
+                            print(f"Ошибка в кодировке {e} {link['url']} {encoding} {response.headers['Content-Type']}")
+                        try:
+                            if self.parse:
+                                await self.getLinks(html, link)
+                        except Exception as e:
+                            print(f"Ошибка в парсинге {e} {link['url']}")
+                        try:
+                            if self.pattern:
+                                await self.search(html, link)
+                        except Exception as e:
+                            print(f"Ошибка в поиске {e} {link['url']}")
+                except Exception as e:
+                    self.links['errors'].append({"url": link["url"], "error": str(e)})
+                    print({"url": link["url"], "error": str(e)})
+
+    async def getLinks(self, html, link):
+        tree = etree.parse(StringIO(html), parser=self.parser)
+        a_tags = tree.xpath("//a[@href]")
+        for a in a_tags:
+            url = a.get("href", "")
+            if url in ("", "/", link["url"], link["url"] + "/", link["url"].replace(self.site, "")) or ("#" in url) or (
+                    "?" in url):
+                continue
+            if url.startswith("/"):
+                url = self.site + url
+                putInDict(url, link, self.links['internal'])
+            elif url.startswith("http"):
+                putInDict(url, link, self.links['external'])
+            else:
+                putInDict(url, link, self.links['resources'])
+
+    async def search(self, html, link):
+        html = html.lower()
+        soup = BeautifulSoup(html, "lxml")
+        html1 = soup.find('div', attrs={'id': 'block_content'})
+        html2 = soup.find('div', attrs={'class': 'slick-track'})
+        html3 = soup.find('div', attrs={'class': 'course-objective-inf'})
+        html4 = soup.find('div', attrs={'class': 'header-icons22'})
+        for p in self.pattern:
+            if p.lower() in (str(html1)+str(html2)+str(html3)+str(html4)).lower():
+                self.result[p].append(link["url"])
+                print({p: link["url"]})
+
+async def main():
     async with aiohttp.ClientSession() as session:
-        i = 0
-        for link in links:
-            try:
-                # менять
-                async with session.get(link["url"]) as response:
-                    print(i / len(links) * 100.0)
-                    i = i + 1
-                    parser = etree.HTMLParser()
-                    content = await response.content.read()
-                    txt = content.decode(encoding, errors='ignore')
-                    for p in pattern:
-                        if p in txt.lower():
-                            if p == "рассрочка":
-                                # менять
-                                if len([el for el in find_all(txt.lower(), p)]) < 3:
-                                    continue
-                            if p in ["справк", "копи", "продлить", "увеличить", "рассрочка"]:
-                                if any([(el in link["url"]) for el in ["/seminar", "/anons"]]):
-                                    continue
-                            if p == "4000":
-                                if (len([el for el in find_all(txt.lower(), p)]) == 1) and ("}, 4000" in txt.lower()):
-                                    continue
-                            res[p].append(link["url"])
-                            print({p: link["url"]})
-            except Exception as e:
-                print(f"Error {e}")
-    return res
+        async with session.get('https://niidpo.ru/seminar/4178') as response:
+            header = response.headers["Content-Type"]
+            if "text/html" not in header:
+                return 0
+            encoding = header[header.find("=") + 1:]
+            html = await response.text(encoding, errors="ignore")
+            html = html.lower()
+            soup = BeautifulSoup(html, "lxml")
+            html1 = soup.find('div', attrs={'id': 'block_content'})
+            html2 = soup.find('div', attrs={'class': 'slick-track'})
+            html3 = soup.find('div', attrs={'class': 'course-objective-inf'})
+            html4 = soup.find('div', attrs={'class': 'header-icons22'})
+            for p in ['бессрочн', 'библиоклуб', 'biblioclub']:
+                if p.lower() in (str(html1)+str(html2)+str(html3)+str(html4)).lower():
+                    print(p)
 
 
-def main(site, encoding, pattern):
-    parser = aioparser()
-    parser.getAllUrls(site, adaptive=True)
-    res = asyncio.run(searcher(parser.links, pattern, encoding))
 
-    with open(autotest_results + f"/{site.replace('https://', '').replace('https://', '')}{fname_appendix}_result.json",
-              "w", encoding="utf-8") as w:
-        json.dump(res, w, indent=4, ensure_ascii=False)
-    with open(autotest_results + f"/{site.replace('https://', '').replace('https://', '')}{fname_appendix}_result.txt",
-              "w") as w:
-        data = []
-        for key in res:
-            for url in res[key]:
-                if url not in data:
-                    data.append(url + "\n")
-        w.writelines(data)
-
-
-if __name__ == "__main__":
-    # res = asyncio.run(searcher([{"url": "https://niidpo.ru/korporativnoe_obuchenie"}], ["4000"], "windows-1251"))
-    #менять
-    fname_appendix = "_adaptive" if True else ""
-    pattern = ["следующей"]
-    main(sys.argv[1], "windows-1251", pattern)
+if __name__ == '__main__':
+    parser = aioparser('https://niidpo.ru', ['бессрочн', 'библиоклуб', 'biblioclub'], parse=True)
+    asyncio.run(parser.run())
+    # asyncio.run(main())
